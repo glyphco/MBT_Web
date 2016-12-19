d36bc4df91e941497f11eedc0da28fa576153e97
Shawn Dalton <sdalton@aroptions.com>
2016-12-18 21:10:51 -0600

    more

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -8,11 +8,31 @@ use Socialite;
 use Redirect;
 
 class SocialAuthController extends Controller {
+
+   protected $request;
+
+   public function __construct(\Illuminate\Http\Request $request)
+   {
+       $this->request = $request;
+   }
+
 	public function redirect($service) {
-		return Socialite::driver ( $service )->redirect ();
+		return Socialite::driver ( $service )->stateless()->redirect();
 	}
+
 	public function callback($service) {
-		$user = Socialite::with ( $service )->user ();
+	$request = $this->request->all();
+
+	if (array_key_exists('doit', $request)) {
+		$user = Socialite::driver($service)->userFromToken($request['token']);
 		return view ( 'home' )->withDetails ( $user )->withService ( $service );
+	}
+
+$user = Socialite::with ( $service )->stateless()->user ();
+
+	$away = 'http://mbtauth.dev/callback/'.$service.'?doit&token='.$user->token;
+
+	return redirect()->away($away);
+
 	}
 }
