5db0f89572a580eb3dfa414323594ec2650e7127
Shawn Dalton <sdalton@aroptions.com>
2016-12-26 17:09:24 -0600

    fixes to JWT retrival

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -6,44 +6,45 @@ use Illuminate\Http\Request;
 use Redirect;
 use Socialite;
 
-class SocialAuthController extends Controller
-{
+class SocialAuthController extends Controller {
 
-    protected $request;
+	protected $request;
 
-    public function __construct(\Illuminate\Http\Request $request)
-    {
-        $this->request = $request;
-    }
+	public function __construct(\Illuminate\Http\Request $request) {
+		$this->request = $request;
+	}
 
-    public function redirect($service)
-    {
-        //return Socialite::driver($service)->stateless()->redirect();
-        return Socialite::driver($service)->redirect();
-    }
+	public function redirect($service) {
+		//return Socialite::driver($service)->stateless()->redirect();
+		return Socialite::driver($service)->redirect();
+	}
 
-    public function callback($service)
-    {
-        //$user = Socialite::with($service)->stateless()->user();
-        $user = Socialite::with($service)->user();
+	public function callback($service) {
+		//$user = Socialite::with($service)->stateless()->user();
+
+		//dd('here');
+		$user = Socialite::with($service)->user();
 
 //get token through guzzle (todo) (for now redirect to auth server)
-        // $authserver = env('AUTH_SERVER', 'http://mbtweb.dev');
-        // $away       = $authserver . '/gettoken/' . $service . '?alias=' . $alias . '&token=' . $user->token;
-        //return redirect()->away($away);
+		// $authserver = env('AUTH_SERVER', 'http://mbtweb.dev');
+		// $away       = $authserver . '/gettoken/' . $service . '?alias=' . $alias . '&token=' . $user->token;
+		//return redirect()->away($away);
 
-        $client = new Client([
-            'base_uri' => env('AUTH_SERVER', 'http://mbtweb.dev'),
-            'timeout'  => 5.0,
-        ]);
+		$client = new Client([
+			'base_uri' => env('AUTH_SERVER', 'http://mbtauth.dev'),
+			'timeout' => 5.0,
+		]);
 
-        $response = $client->request('GET', '/gettoken/' . $service, [
-            'query' => ['token' => $user->token],
-        ]);
+		$response = $client->request('GET', '/gettoken/' . $service, [
+			'query' => ['token' => $user->token],
+		]);
 
-        foreach ($response->getHeaders() as $name => $values) {
-            echo $name . ':::: ' . implode(', ', $values) . "<br>";
-        }
-    }
+		// foreach ($response->getHeaders() as $name => $values) {
+		// 	echo $name . ':::: ' . implode(', ', $values) . "<br>";
+		// }
+		$contents = $response->getBody()->getContents();
+		$jwt = json_decode($contents, true)['JWT'];
+		dd($jwt);
+	}
 
 }
--- config/services.php
+++ config/services.php
@@ -1,25 +1,25 @@
 <?php
 return [
-    'facebook' => [
-        'client_id'     => env('FACEBOOK_ID'),
-        'client_secret' => env('FACEBOOK_SCECRET'),
-        'redirect'      => env('FACEBOOK_REDIRECT'),
-    ],
-    'google'   => [
-        'client_id'     => env('GOOGLE_ID'),
-        'client_secret' => env('GOOGLE_SECRET'),
-        'redirect'      => env('GOOGLE_REDIRECT'),
-    ],
-    // 'twitter' => [
+	'facebook' => [
+		'client_id' => env('FACEBOOK_ID'),
+		'client_secret' => env('FACEBOOK_SECRET'),
+		'redirect' => env('FACEBOOK_REDIRECT'),
+	],
+	'google' => [
+		'client_id' => env('GOOGLE_ID'),
+		'client_secret' => env('GOOGLE_SECRET'),
+		'redirect' => env('GOOGLE_REDIRECT'),
+	],
+	// 'twitter' => [
 
-    //         'client_id' => env ( 'TWITTER_CLIENT_ID' ),
-    //         'client_secret' => env ( 'TWITTER_CLIENT_SECRET' ),
-    //         'redirect' => env ( 'TWITTER_REDIRECT' )
-    // ],
-    // 'github' => [
+	//         'client_id' => env ( 'TWITTER_CLIENT_ID' ),
+	//         'client_secret' => env ( 'TWITTER_CLIENT_SECRET' ),
+	//         'redirect' => env ( 'TWITTER_REDIRECT' )
+	// ],
+	// 'github' => [
 
-    //         'client_id' => env ( 'GITHUB_CLIENT_ID' ),
-    //         'client_secret' => env ( 'GITHUB_CLIENT_SECRET' ),
-    //         'redirect' => env ( 'GITHUB_REDIRECT' )
-    // ]
+	//         'client_id' => env ( 'GITHUB_CLIENT_ID' ),
+	//         'client_secret' => env ( 'GITHUB_CLIENT_SECRET' ),
+	//         'redirect' => env ( 'GITHUB_REDIRECT' )
+	// ]
 ];
