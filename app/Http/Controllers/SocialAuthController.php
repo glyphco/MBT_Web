c7ab1c1791d097bb03e54654574e00349e9e89a5
AROPTIONS\sdalton <glypher@gmail.com>
2017-01-04 18:24:31 -0600

    formatting changes

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -1,4 +1,5 @@
-<?php namespace App\Http\Controllers;
+<?php
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use GuzzleHttp\Client;
@@ -6,51 +7,47 @@ use Illuminate\Http\Request;
 use Redirect;
 use Socialite;
 
-class SocialAuthController extends Controller
-{
+class SocialAuthController extends Controller {
 
-    protected $request;
+	protected $request;
 
-    public function __construct(\Illuminate\Http\Request $request)
-    {
-        $this->request = $request;
-    }
+	public function __construct(\Illuminate\Http\Request $request) {
+		$this->request = $request;
+	}
 
-    public function redirect($service)
-    {
-        //return Socialite::driver($service)->stateless()->redirect();
-        return Socialite::driver($service)->redirect();
-    }
+	public function redirect($service) {
+		//return Socialite::driver($service)->stateless()->redirect();
+		return Socialite::driver($service)->redirect();
+	}
 
-    public function callback($service)
-    {
-        //create a temporary socailuser from the data we just received from the service (stateless because we arent validating state)
-        $socailuser = Socialite::with($service)->stateless()->user();
+	public function callback($service) {
+		//create a temporary socailuser from the data we just received from the service (stateless because we arent validating state)
+		$socailuser = Socialite::with($service)->stateless()->user();
 
-        //$socailuser = Socialite::with($service)->user();
+		//$socailuser = Socialite::with($service)->user();
 
-        $client = new Client([
-            'base_uri' => env('AUTH_SERVER', 'http://mbtauth.dev'),
-            'timeout'  => 5.0,
-        ]);
+		$client = new Client([
+			'base_uri' => env('AUTH_SERVER', 'http://mbtauth.dev'),
+			'timeout'  => 5.0,
+		]);
 
-        $response = $client->request('GET', '/gettoken/' . $service, [
-            'query' => ['token' => $socailuser->token],
-        ]);
+		$response = $client->request('GET', '/gettoken/' . $service, [
+			'query' => ['token' => $socailuser->token],
+		]);
 
-        // foreach ($response->getHeaders() as $name => $values) {
-        //     echo $name . ':::: ' . implode(', ', $values) . "<br>";
-        // }
-        $contents = $response->getBody()->getContents();
-        $jwt      = json_decode($contents, true)['JWT'];
-        //dd($jwt);
+		// foreach ($response->getHeaders() as $name => $values) {
+		//     echo $name . ':::: ' . implode(', ', $values) . "<br>";
+		// }
+		$contents = $response->getBody()->getContents();
+		$jwt      = json_decode($contents, true)['JWT'];
+		//dd($jwt);
 
-        $cookie = cookie('token', $jwt, config('jwt.ttl'), "/", null, false, true);
+		$cookie = cookie('token', $jwt, config('jwt.ttl'), "/", null, false, true);
 
-        //return response()->json($jwt)->withCookie($cookie);
+		//return response()->json($jwt)->withCookie($cookie);
 
-        return redirect('home')->withCookie($cookie);
+		return redirect('home')->withCookie($cookie);
 
-    }
+	}
 
 }
